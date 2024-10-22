0174799387e2612c92fb1d236b9ab69ff6a8c22b
Holly <holly@dejawu.me>
2024-10-22 16:10:28 -0700

    Event collection and element insertion

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,6 +2,20 @@
 /** @typedef {ActivityResponse[number]} Event */
 /** @typedef {Event['repo']} Repo */
 
+/**
+ *
+ * @param {string} id
+ * @returns {HTMLElement}
+ */
+const assertElement = (id) => {
+  const el = document.getElementById(id);
+  if (!el) {
+    throw new Error("Missing required element.");
+  }
+
+  return el;
+};
+
 /** @returns {Promise<ActivityResponse>} */
 const fetchEvents = async () => {
   const resp = await fetch(
@@ -26,27 +40,69 @@ const EVENT_TYPES = [
   "ReleaseEvent",
 ];
 
+const SHOWN_REPOS = 3;
+const SHOWN_EVENTS = 15;
+
 const events = (await fetchEvents()).filter((event) =>
   EVENT_TYPES.includes(event.type || "")
 );
 
-const repos = events.reduce(
-  /**
-   *
-   * @param {Record<string, Event[]>} acc
-   * @param {Event} event
-   */
-  (acc, event) => {
-    const repo = event.repo.name;
-    if (!acc[repo]) {
-      acc[repo] = [];
-    }
+const repos = Object.entries(
+  events.reduce(
+    /**
+     *
+     * @param {Record<string, Event[]>} acc
+     * @param {Event} event
+     */
+    (acc, event) => {
+      const repo = event.repo.name;
+      if (!acc[repo]) {
+        acc[repo] = [];
+      }
 
-    acc[repo].push(event);
+      acc[repo].push(event);
 
-    return acc;
-  },
-  {}
-);
+      return acc;
+    },
+    {}
+  )
+)
+  .filter(([k, v]) => v.length >= 2)
+  .map(
+    /**
+     *
+     * @param {[string, Event[]]} _
+     * @returns {[string, Event[]]}
+     */
+    ([k, v]) => [k, v.slice(0, SHOWN_EVENTS)]
+  )
+  .slice(0, SHOWN_REPOS);
+
+const $activityGroups = assertElement("activity-groups");
 
 console.log(repos);
+
+repos.forEach(([name, entries]) => {
+  const $activity = document.createElement("div");
+  $activity.setAttribute("class", "activity-group");
+
+  const $header = document.createElement("h1");
+  $header.innerText = name;
+  $activity.append($header);
+
+  entries.forEach((entry) => {
+    const $entry = document.createElement("div");
+    $entry.append(
+      (() => {
+        const $event = document.createElement("p");
+        $event.innerText = entry.type || "";
+
+        return $event;
+      })()
+    );
+
+    $activity.append($entry);
+  });
+
+  $activityGroups.append($activity);
+});
