6751033f79e9e5146bb04a5a12eebed82bae91cd
Holly <holly@dejawu.me>
2024-11-07 23:08:57 -0800

    View layer and some proof-of-concept commands

--- src/pages/term.js
+++ src/pages/term.js
@@ -1,19 +1,75 @@
-const $templatePrompt = /** @type {HTMLTemplateElement} */ (
-  document.querySelector("#template-prompt")
-);
-const $terminal = /** @type {HTMLElement} */ (
-  document.querySelector("#terminal")
-);
+/** @param {{ onInput: (input: string) => void}} onInput */
+const view = ({ onInput }) => {
+  const $templatePrompt = /** @type {HTMLTemplateElement} */ (
+    document.querySelector("#template-prompt")
+  );
+  const $terminal = /** @type {HTMLElement} */ (
+    document.querySelector("#terminal")
+  );
+  let $currentPrompt = /** @type {HTMLSpanElement | null} */ (null);
+
+  document.addEventListener("click", (ev) => {
+    if (ev.target === document.querySelector("html")) $currentPrompt?.focus();
+  });
+
+  return {
+    /** @param {HTMLElement} $el */
+    append: function ($el) {
+      $terminal.append($el);
+    },
+    prompt: function () {
+      const $prompt = /** @type {HTMLElement} */ (
+        $templatePrompt.content.cloneNode(true)
+      );
+      const $promptInput = /** @type {HTMLSpanElement} */ (
+        $prompt.querySelector(".prompt-input")
+      );
+      $promptInput.addEventListener("keydown", (ev) => {
+        if (ev.ctrlKey && ev.key === "c") {
+          $promptInput.removeAttribute("contenteditable");
+          this.prompt();
+          return;
+        }
+
+        if (ev.key === "Enter") {
+          ev.preventDefault();
+          onInput($promptInput.innerText);
+          this.prompt();
+          return;
+        }
+      });
+
+      $terminal.append($prompt);
+      $currentPrompt = $promptInput;
+      $promptInput.focus();
+    },
+  };
+};
 
 class Terminal {
   constructor() {
-    this.$term = document.getElementById("terminal");
-    this.$prompt = document.querySelector(".prompt");
-    console.log(this.$prompt);
-  }
+    this.view = view({
+      onInput: (input) => {
+        const argv = input.split(" ");
+        const command = argv[0];
 
-  prompt() {
-    const $prompt = $templatePrompt.content.cloneNode();
+        switch (command) {
+          case "whoami":
+            {
+              const $span = document.createElement("span");
+              $span.innerText = "madison";
+              this.view.append($span);
+            }
+            break;
+          default: {
+            const $span = document.createElement("span");
+            $span.innerText = `Unknown command: ${command}`;
+            this.view.append($span);
+          }
+        }
+      },
+    });
+    this.view.prompt();
   }
 }
 
